a0fc49efee7c6f1f535bc1bf2728043af1cf4af1
Leonardo Crestani <leonardo.crestani@hotmail.com>
2022-04-07 11:27:25 -0300

    feat: Adding validator for method findById in route 'car' and implementing method in router

--- src/routes/carRoutes.ts
+++ src/routes/carRoutes.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import CarController from '../controllers/CarController';
 import findValidator from '../validators/Car/find';
+import findByIdValidator from '../validators/Car/findById';
 import registerValidator from '../validators/Car/register';
 import updateValidator from '../validators/Car/update';
 import deleteValidator from '../validators/Car/delete';
 const router = express.Router();
 
 router.get('/cars', findValidator, CarController.find);
+router.get('/cars/:id', findByIdValidator, CarController.findById);
 router.post('/cars', registerValidator, CarController.register);
 router.put('/cars/:id', updateValidator, CarController.update);
 router.delete('/cars/:id', deleteValidator, CarController.remove);
