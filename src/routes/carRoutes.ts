2390088601333656a1fa79e86e5506dc780dd73e
Leonardo Crestani <leonardo.crestani@hotmail.com>
2022-04-07 10:07:22 -0300

    Implementing method Put in route 'car'

--- src/routes/carRoutes.ts
+++ src/routes/carRoutes.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import CarController from '../controllers/CarController';
 import findValidator from '../validators/Car/find';
 import registerValidator from '../validators/Car/register';
+import updateValidator from '../validators/Car/update';
 import deleteValidator from '../validators/Car/delete';
 const router = express.Router();
 
 router.get('/cars', findValidator, CarController.find);
 router.post('/cars', registerValidator, CarController.register);
+router.put('/cars/:id', updateValidator, CarController.update);
 router.delete('/cars/:id', deleteValidator, CarController.remove);
 
 export default router;
