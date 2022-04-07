e07fc5c9f333f23d119357e38eebc97ce9913399
Leonardo Crestani <leonardo.crestani@hotmail.com>
2022-04-07 10:03:47 -0300

    Implementing method Delete in route 'car'

--- src/routes/carRoutes.ts
+++ src/routes/carRoutes.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import CarController from '../controllers/CarController';
 import findValidator from '../validators/Car/find';
 import registerValidator from '../validators/Car/register';
+import deleteValidator from '../validators/Car/delete';
 const router = express.Router();
 
 router.get('/cars', findValidator, CarController.find);
 router.post('/cars', registerValidator, CarController.register);
+router.delete('/cars/:id', deleteValidator, CarController.remove);
 
 export default router;
