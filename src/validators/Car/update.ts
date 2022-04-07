8c17dd72e82aec9950f8d3465476d5a214911e70
Leonardo Crestani <leonardo.crestani@hotmail.com>
2022-04-07 10:06:36 -0300

    feat: Implementing validator for method update in route 'car'

---
+++ src/validators/Car/update.ts
@@ -0,0 +1,25 @@
+import Joi from 'joi';
+import { Request, Response, NextFunction } from 'express';
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const schema = Joi.object({
+            model: Joi.string().trim().optional(),
+            color: Joi.string().trim().optional(),
+            year: Joi.number().optional(),
+            accessories: Joi.array().items(Joi.object({ description: Joi.string().trim().required() }).required()).unique('description').optional(),
+            passengers: Joi.number().optional(),
+        }).min(1);
+        const { error } = await schema.validate(req.body, { abortEarly: true });
+        if (error) throw error
+        return next();
+    }
+    catch (erro) {
+        return res.status(400).json(
+            erro.details.map((detail: any) => ({
+                message: detail.message,
+                path: detail.path
+            }))
+        )
+    }
+}
