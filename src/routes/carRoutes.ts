7bafdb6d4eede5a15c6c77c3c605d92a901700ad
Leonardo Crestani <leonardo.crestani@hotmail.com>
2022-04-07 09:52:26 -0300

    Implementing method Post in route 'Car'

--- src/routes/carRoutes.ts
+++ src/routes/carRoutes.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import CarController from '../controllers/CarController';
 import findValidator from '../validators/Car/find';
+import registerValidator from '../validators/Car/register';
 const router = express.Router();
 
 router.get('/cars', findValidator, CarController.find);
+router.post('/cars', registerValidator, CarController.register);
 
 export default router;
